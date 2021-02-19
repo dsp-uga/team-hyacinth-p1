50f1bd062ab7fe9fe867d00868816d9ea0878d84
YDivyaKrishna <divyakrishna0497@gmail.com>
2021-02-18 19:36:26 -0500

    Training_model

--- src/random_forest.py
+++ src/random_forest.py
@@ -3,6 +3,18 @@ from pyspark import SparkContext
 from pyspark import SparkConf
 from pyspark.sql.session import SparkSession
 import sys
+from pyspark.sql import Row
+from pyspark.sql.functions import udf
+from pyspark.ml.feature import CountVectorizer
+import re
+from pyspark.sql.types import ArrayType, StringType
+import pickle
+from pyspark.ml.feature import VectorAssembler
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.tree import GradientBoostedTrees
+from pyspark.mllib.tree import RandomForest
+import pandas as pd
+import pandas
 
 # creating the spark context
 sc = SparkContext.getOrCreate()
@@ -12,27 +24,23 @@ spark = SparkSession(sc)
 def byte_feature_extraction(lists, file, feature, track, value):
 
         for hexa_file_num, f in enumerate(file):
-        print(hexa_file_num)
-        inputs = requests.get("https://storage.googleapis.com/uga-dsp/project1/data/bytes/" + f +".bytes", stream=True)
-        for hexa_file in inputs.iter_hexa_files():
-            if len(hexa_file) == 0:
-                continue
-            hexa_file = hexa_file.split()
-            
-            for byte in hexa_file[1:]:
-                if '?' in byte:
+            inputs = requests.get("https://storage.googleapis.com/uga-dsp/project1/data/bytes/" + f +".bytes", stream=True)
+            for hexa_file in inputs.iter_lines():
+                if len(hexa_file) == 0:
                     continue
-                else:
-                    if byte not in track:
-                        if value == 1:
-                            continue
+                hexa_file = hexa_file.split()
+            
+                for byte in hexa_file[1:]:
+                        if byte not in track:
+                            if value == 1:
+                                continue
+                            else:
+                                track[byte] = feature
+                                lists[hexa_file_num][feature] += 1
+                                feature += 1
                         else:
-                            track[byte] = index
-                            lists[hexa_file_num][feature] += 1
-                            feature += 1
-                    else:
-                        X[hexa_file_num][track[byte]] += 1
-    return lists, feature, track
+                                lists[hexa_file_num][track[byte]] += 1
+        return lists, feature, track
 
 def feature_extraction(train_path, test_path, max_val):
     #feature extraction to extract the count of bytes from the files
@@ -41,18 +49,18 @@ def feature_extraction(train_path, test_path, max_val):
     track_files = {}
     training = 0
     testing = 1
-    train = [[0]*max_val for x in range(length_of_train)]
-    test = [[0]*max_val for x in range(length_of_test)]
     train_file = requests.get(train_path, stream=True)
     test_file = requests.get(test_path, stream=True)  
     train_files = get_files(train_file)
     test_files = get_files(test_file)   
     length_of_train = len(train_files)
     length_of_test = len(test_files)
+    train = [[0]*max_val for x in range(length_of_train)]
+    test = [[0]*max_val for x in range(length_of_test)]
     train, current_feature, track_files = byte_feature_extraction(train[:], train_files, current_feature, track_files, training)
     test, current_feature, track_files = byte_feature_extraction(test[:], test_files, current_feature, track_files, testing)
-    train = [t[:index] for t in train[:]]
-    test = [t[:index] for t in test[:]]
+    train = [t[:current_feature] for t in train[:]]
+    test = [t[:current_feature] for t in test[:]]
     with open('train_set', 'wb') as fp:
         pickle.dump(train, fp)      
     with open('test_set', 'wb') as fp:
@@ -62,8 +70,8 @@ def feature_extraction(train_path, test_path, max_val):
 
 def get_files(files):
     file_names = []
-    for f in files.iter_hexa_files():
-        file_names.append()
+    for f in files.iter_lines():
+        file_names.append(f)
     return file_names    
     
 def add_labels(y_path):
@@ -75,6 +83,23 @@ def add_labels(y_path):
 
     return y_train
 
+def generate_training_matrix(X_train, y_train):
+    
+    training_data = []
+    length_of_train = len(X_train)    
+    for index in range(length_of_train):
+        training_data.append(LabeledPoint(y_train[index], X_train[index]))
+    return training_data
+
+def malware_predict_and_store(sc, training_set, X_test, num_of_trees, depth):
+    
+    classifier_model = RandomForest.trainClassifier(sc.parallelize(training_set), 9, {}, num_of_trees,  maxDepth = depth)
+    result = []
+    for index in X_test:
+         result.append(int(classifier_model.predict(index) + 1))
+    name = 'result.txt'
+    df = pd.DataFrame(result)
+    df.to_csv(name, header = False, index = False)
 
 def main():
 
@@ -97,8 +122,14 @@ def main():
     y_train = add_labels(y_path)
     
     #feature-extraction
+
     X_train, X_test = feature_extraction(X_train_path, X_test_path, max_val)
 
+    #generating training data
+    training_set = generate_training_matrix(X_train, y_train)
+    
+    #predicting the malware using RandomForest Classfier and saving the output in the GS bucket
+    malware_predict_and_store(sc, training_set, X_test, num_of_trees, depth)
 
     #print(y_train)
 if __name__ == "__main__":
